cc514e3e5f2689742f36ca8ffb51d208fd1749a7
orangelight <alex.deneau17@gmail.com>
2014-12-31 20:30:55 -0500

    Added relations

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,2 +1,4 @@
 class Signup < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :event
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :events, :through => :signups
+  has_many :meetings, :through => :attendances
+  has_many :strikes 
 end
