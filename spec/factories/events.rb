844199094fa113d60c006fef35592b40b3aac4e6
John DeSilva <desilvjo@umich.edu>
2015-01-02 15:11:21 -0500

    Update events factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,11 +3,11 @@ FactoryGirl.define do
     title "MyString"
     descriprion "MyString"
     max_slots 1
-    event_date "2014-12-31"
-    signup_deadline_date "MyString"
+    event_date Time.now
+    signup_deadline_date { Time.now + 5.minutes }
     credits 1.5
-    start_time "2014-12-31 20:04:30"
-    end_time "2014-12-31 20:04:30"
+    start_time { Time.now }
+    end_time { Time.now + 30.minutes }
   end
 
 end
