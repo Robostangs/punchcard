3466accb8cd5cb8812ee0923a0305f26958564a4
orangelight <alex.deneau17@gmail.com>
2015-01-02 15:26:32 -0500

    Added signup and backout to event controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,32 @@
+class EventsController < ApplicationController
+  def create
+  end
+  def new
+  end
+  def show
+  end
+  def edit
+  end
+  def destroy
+  end
+  def singup
+    @event = Event.where(id: params[:id])
+    if current_user.signup_for(@event)
+      flash[:notice] = "Signed Up for Event"
+      redirect_to event_path(@event)
+    else
+      flash[:error] = "Could Not Sign Up for Event"
+      redirect_to event_path(@event)
+    end
+  end
+  def signdown
+    @event = Event.where(id: params[:id])
+    if current_user.backout_from(@event)
+      flash[:notice] = "Succesufly Backed Out!"
+      redirect_to event_path(@event)
+    else
+      flash[:error] = "Back Out Deadline has Passed!"
+      redirect_to event_path(@event)
+    end
+  end
+end
