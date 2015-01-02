607c8833287a78649517d142896f5c607c3892ab
John DeSilva <desilvjo@umich.edu>
2015-01-02 14:54:27 -0500

    Update Event factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
-  factory :event, :class => 'Events' do
+  factory :event, :class => 'Event' do
     title "MyString"
-descriprion "MyString"
-max_slots 1
-event_date "2014-12-31"
-signup_deadline_date "MyString"
-credits 1.5
-start_time "2014-12-31 20:04:30"
-end_time "2014-12-31 20:04:30"
+    descriprion "MyString"
+    max_slots 1
+    event_date "2014-12-31"
+    signup_deadline_date "MyString"
+    credits 1.5
+    start_time "2014-12-31 20:04:30"
+    end_time "2014-12-31 20:04:30"
   end
 
 end
