d2cfdf7aa10f362089807e3746ad0a664cd1686c
Nicholas Yang <parablank@gmail.com>
2015-01-02 23:47:50 -0500

    i'm dumb, authenticate_admin should be working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+  before_action :authenticate_user!
+
   def authenticate_admin!
-    unless current_user.admin?
+    if current_user.admin == false
+      flash[:alert] = "You are not an admin. Redirected."
       redirect_to new_user_session_path
-      if current_user.nil
-        flash[:alert] = "Please log in."
-      else
-        flash[:alert] = "You are not an admin. Redirected."
-      end
     end
   end
 end
