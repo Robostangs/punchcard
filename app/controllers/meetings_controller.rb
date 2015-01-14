ebce835f855f476655723223a3f17ed2274c3e2b
Nicholas Yang <parablank@gmail.com>
2015-01-13 19:22:12 -0500

    barcode scanner integration, small changes
    1. a br in the users show view on the leaderboards
    2. auto select on meetings#check for barcode scanner integration
    3. remove s on user id in meetings controller

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -6,6 +6,9 @@ class MeetingsController < ApplicationController
   def check
     @meeting = Meeting.find(params[:id])
     student_id = params['student_id']
+    if student_id.to_s.chr == 'S'
+      student_id.delete! 'S'
+    end
     if not student_id == nil
       flashAttendance = @meeting.take_attendance(@meeting, student_id).to_s
       if flashAttendance == 'User does not exist.'
